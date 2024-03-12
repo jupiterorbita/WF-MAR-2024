0a827324c526932ce00cd61d64793e4034e9e1b6
JupiterOrbita <38020888+jupiterorbita@users.noreply.github.com>
2024-03-12 11:14:42 -0600

    add w1d2 algos and CSS intro

---
+++ w1d2.js
@@ -0,0 +1,40 @@
+var wordArray = ['shawn', 'jim', 'tyler', 'heidi', 'john', 'alfredo', 'michael']
+
+/**
+ * Determines the average length of the words in the given array.
+ * @returns {number} The average length of the given words.
+ */
+function avgWordLength(arr) {
+    // create a sum var and set it equal = 0;
+    // create a length var = arr.length // 7
+    // loop over the arr array
+        // arr[i]
+        // add to sum the arr[i]'s length
+    // do something else...
+
+}
+
+console.log(avgWordLength(wordArray))
+
+/**
+ * Finds the longest word in the given array of words.
+ * @returns {string} The longest word. If there are multiple words with the same
+ *    length, this should be the last word in the array with that length.
+ */
+function findLongestWord(arr) {}
+
+console.log(findLongestWord(wordArray))
+
+/************************************************************************************************/
+
+var rangeA1 = 10
+var rangeB1 = 15
+var rangeExpected = 75
+/**
+ * Calculates the sum of the given range, inclusive. I.e., the sum of the first
+ * number through the last number, inclusive.
+ * @returns {number} The sum of the given range, inclusive.
+ */
+function inclusiveRangeSum(start, stop) {}
+
+console.log(inclusiveRangeSum(rangeA1, rangeB1))
